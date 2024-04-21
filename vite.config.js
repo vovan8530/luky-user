b4cae3aaf8d32858292a0c61dc33be151c660d28
vovan8530 <vovan.b8530@gmail.com>
2024-04-21 14:09:55 +0300

    viteStaticCopy

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-
-// vite.config.js / vite.config.ts
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-
-
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/css\app.css',
+                'resources/js\app.js',
             ],
             refresh: true,
-        }),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'bin/example.wasm',
-                    dest: 'wasm-files'
-                }
-            ]
         }),
     ],
 });
